89991ea16b3b6eb2ddcf7c92eec291d74faa04a4
magmadon <babyboydu91@live.fr>
2019-09-03 19:43:25 +0200

    Finition de l'analyseur de lexique

--- lexical.py
+++ lexical.py
@@ -1,39 +1,72 @@
 
 les_tokens = []
 
-fichier = open("bidule.txt","r")
-content = ficher.read()
-
+fichier = open("test.c","r")
+content = fichier.read()
+print(content)
+index_token = -1
 num_lettre = 0
 num_lig = 1
 
-index_token = 0
-
 def next():
-    return les_tokens[index_token]
+    if index_token != -1:
+        return les_tokens[index_token]
+    else:
+        return None
 
 def skip():
-    index_token += 1
+    global index_token
+    index_token = index_token + 1
 
 def main():
-    while(num_lettre < len(content)):
-        if content[num_lettre] != " " and (content[num_lettre] != "\\" and next() != "n"):
-            tokens(content[num_lettre], num_lig)
+    global num_lettre
+    global num_lig
+    while(True):
+        if next() != None:
+            if next()['type'] == "tok_EOF":
+                for elem in les_tokens:
+                    print(elem['type'])
+                break
+        if content[num_lettre] != " " and content[num_lettre] != "\n":
+            tokens(content[num_lettre])
             skip()
         else:
-            if content[num_lettre] != "\\" and next() != "n":
-                skip()
-                skip()
+            if content[num_lettre] == "\n":
                 num_lig += 1
-            else:
-                skip()
+            num_lettre += 1
 
 
-def tokens(c, num_lig):
+def tokens(c):
+    global num_lettre
+    global num_lig
     if c == "+":
         les_tokens.append({'type':'tok_plus', "ligne": num_lig})
+        num_lettre += 1
     elif c == "-":
         les_tokens.append({'type':'tok_moins', "ligne": num_lig})
+        num_lettre += 1
+    elif c == "=":
+        if content[num_lettre+1] == "=":
+            les_tokens.append({'type':'tok_egal', "ligne": num_lig})
+            num_lettre += 2
+        else:
+            les_tokens.append({'type':'tok_affectation', "ligne": num_lig})
+            num_lettre += 1
+    elif c == "(":
+        les_tokens.append({'type':'tok_parenthèse_ouvrante', "ligne": num_lig})
+        num_lettre += 1
+    elif c == ")":
+        les_tokens.append({'type':'tok_parenthèse_fermante', "ligne": num_lig})
+        num_lettre += 1
+    elif c == "{":
+        les_tokens.append({'type':'tok_accolade_ouvrante', "ligne": num_lig})
+        num_lettre += 1
+    elif c == "}":
+        les_tokens.append({'type':'tok_accolade_fermante', "ligne": num_lig})
+        num_lettre += 1
+    elif c == ";":
+        les_tokens.append({'type':'tok_point_virgule', "ligne": num_lig})
+        num_lettre += 1
     elif c.isdigit():
         i = num_lettre+1
         while(content[i].isdigit()):
@@ -42,12 +75,16 @@ def tokens(c, num_lig):
         num_lettre = i
         les_tokens.append({'type':'tok_constante', 'valeur': int(c), "ligne": num_lig})
     elif c.isalpha():
-        i = num_lettre+1
+        i = num_lettre + 1
         while(content[i].isalpha()):
             c += content[i]
             i += 1
         num_lettre = i
         les_tokens.append({'type':'tok_identificateur', 'name': c, "ligne": num_lig})
+    if len(content)-1 == num_lettre:
+        les_tokens.append({'type':'tok_EOF', "ligne": num_lig})
+        skip()
+
 
 if __name__ == '__main__':
     main()
