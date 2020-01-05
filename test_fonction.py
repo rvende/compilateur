8558b406e5037ba9c550f1771787137375620ef8
rvende <vende.remi@hotmail.fr>
2020-01-05 18:32:46 +0100

    rapport

--- test_fonction.py
+++ test_fonction.py
@@ -65,7 +65,7 @@ class Test_Fonction(unittest.TestCase):
 		output,error = process.communicate()
 		self.assertEqual(output.decode('utf8'),"1020\n")
 
-	def test_fonction_recursive_trois_arg(self):
+	def test_fonction_recursive_trois_arg_with_loops_and_break(self):
 		f = open("test.c","w+")
 
 		f.write("function somme(n,m,k){ if(n<=0){ return 2+m+k;  }else{ if(n == 15){ return n;} var i; for(i =0;i<3;i=i+1){ n = n-1; if(n == 12){ break; }} return n*m+k + somme(n-1,m,k-1) + k -2; } } function main() { var a; a = 2; a = a+ somme(22,4,2); print(a); return a;}")
@@ -94,7 +94,7 @@ class Test_Fonction(unittest.TestCase):
 		output,error = process.communicate()
 		self.assertEqual(output.decode('utf8'),"218\n")
 
-	def test_fonction_recursive_trois_arg_with_loops_and_break(self):
+	def test_fonction_recursive_trois_arg(self):
 		f = open("test.c","w+")
 
 		f.write("function somme(n,m,k){ if(n<=0){ return 2+m+k;  }else{ return n*m+k + somme(n-1,m,k-1) + k -2; } } function main() { var a; a = 2; a = a+ somme(22,4,2); print(a); return a;}")
